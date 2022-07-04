cb59a9a6aef1a8d1644ad3e316b87d7263021ecf
Yohanes <yohanesabing@gmail.com>
2022-07-04 12:08:43 +0700

    Change api get transactions to get request

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [UserAuthController::class, 'register']);
 Route::post('/login', [UserAuthController::class, 'login']);
-Route::post('/transactions', [UserAuthController::class, 'getTrans'])->middleware('auth:api');
+Route::get('/transactions', [UserAuthController::class, 'getTrans'])->middleware('auth:api');
